f193f845e0ad9dfffec7e5ae4e6ca3adc0febda0
Vamilton <arteiano@yandex.ru>
2022-11-09 21:07:49 +0300

    победа

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,17 +1,25 @@
 import csv
 
 from django.core.management.base import BaseCommand
-from phones.models import Phone
+from django.template.defaultfilters import slugify
 
+from phones.models import Phone
+from django.db import models
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
         pass
-
+#
     def handle(self, *args, **options):
         with open('phones.csv', 'r') as file:
             phones = list(csv.DictReader(file, delimiter=';'))
-
         for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+            _, created = Phone.objects.get_or_create(
+                id = phone['id'],
+                name = phone['name'],
+                price = phone['price'],
+                image = phone['image'],
+                release_date = phone['release_date'],
+                lte_exists = phone['lte_exists'],
+                slug = slugify(phone['name'])
+                )
--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,13 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=250)
+    price = models.IntegerField()
+    image = models.ImageField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(max_length=300, unique=True, db_index=True)
+
+    def __str__(self):
+        return f'{self.name}, {self.price}'
