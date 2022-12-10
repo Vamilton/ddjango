d12f8f87fa0185d0303ec9f62d51d0c5d6251c37
Vamilton <arteiano@yandex.ru>
2022-12-10 15:58:45 +0300

    админка и модели

--- 3.1-drf-intro/smart_home/measurement/admin.py
+++ 3.1-drf-intro/smart_home/measurement/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Sensor, Measurement
 
-# Register your models here.
+@admin.register(Sensor)
+class SensorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'about', 'when']
+
+
+@admin.register(Measurement)
+class MeasurementAdmin(admin.ModelAdmin):
+    list_display = ['sensor', 'date', 'temperature', 'image']
--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+class Sensor(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Датчик')
+    about = models.CharField(max_length=250, verbose_name='Какой')
+    when = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return self.name
+
+
+
+class Measurement(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measurements', verbose_name='Датчик')
+    temperature = models.IntegerField(verbose_name='Температура')
+    date = models.DateTimeField(auto_now_add=True, verbose_name='Дата')
+    image = models.ImageField(blank=True, verbose_name='Фото')
