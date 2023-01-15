1c29d507a401ca0bde4f335b9e13aee9ce99b27c
Vamilton <arteiano@yandex.ru>
2023-01-15 20:51:23 +0300

    15.01

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,31 +1,33 @@
 from rest_framework import serializers
+from .models import *
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
-    pass
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
+
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для позиции продукта на складе
-    pass
+    class Meta:
+        model = StockProduct
+        fields = ['stock', 'product', 'quantity', 'price']
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
-    # настройте сериализатор для склада
-
+    class Meta:
+        model = Stock
+        fields = ['id', 'positions', 'address']
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-
         # создаем склад по его параметрам
         stock = super().create(validated_data)
-
-        # здесь вам надо заполнить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
+        for position in positions:
+            StockProduct.objects.create(stock=stock, **position)
 
         return stock
 
@@ -35,9 +37,15 @@ class StockSerializer(serializers.ModelSerializer):
 
         # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
+        for position in positions:
+            '''defaults – Defines values of fields that should be updated (or created)'''
+            defaults = {'quantity': position['quantity'],
+                'price': position['price']
+            }
+            StockProduct.objects.update_or_create(stock=stock,
+                                                  product=position['product'],
+                                                  defaults=defaults
+                                                  )
 
-        # здесь вам надо обновить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
 
         return stock
